8fb57fc3e69a4bd5396f9004f377b30df0b11d38
arthurgrandao <arthur.gdemello@gmail.com>
2024-09-08 18:10:15 -0300

    refact: troca snippets

--- src/constants.js
+++ src/constants.js
@@ -8,11 +8,10 @@ export const LANGUAGE_VERSIONS = {
 };
 
 export const CODE_SNIPPETS = {
-  javascript: `function greet(name) {\n\tconsole.log("Hello, " + name + "!");\n}\n\ngreet("Alex");\n`,
-  typescript: `type Params = {\n\tname: string;\n}\n\nfunction greet(data: Params) {\n\tconsole.log("Hello, " + data.name + "!");\n}\n\ngreet({ name: "Alex" });\n`,
-  python: `def greet(name):\n\tprint("Hello, " + name + "!")\n\ngreet("Alex")\n`,
-  java: `public class HelloWorld {\n\tpublic static void main(String[] args) {\n\t\tSystem.out.println("Hello World");\n\t}\n}\n`,
-  csharp:
-    'using System;\n\nnamespace HelloWorld\n{\n\tclass Hello { \n\t\tstatic void Main(string[] args) {\n\t\t\tConsole.WriteLine("Hello World in C#");\n\t\t}\n\t}\n}\n',
-  php: "<?php\n\n$name = 'Alex';\necho $name;\n",
+  javascript: `console.log("Hello, World!");`,
+  typescript: `console.log("Hello, World!");`,
+  python: `print("Hello, World!")`,
+  java: `public class HelloWorld {\n\tpublic static void main(String[] args) {\n\t\tSystem.out.println("Hello, World!");\n\t}\n}`,
+  csharp: `using System;\n\nclass Program\n{\n\tstatic void Main(string[] args)\n\t{\n\t\tConsole.WriteLine("Hello, World!");\n\t}\n}`,
+  php: `<?php\n\techo "Hello, World!";\n?>`,
 };
